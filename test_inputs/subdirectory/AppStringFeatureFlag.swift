b6c1581ae0f114213018efe717a57723260caa9d
Lloyd Dapaah <lloyd.lloyddapaah@gmail.com>
2025-01-17 17:51:28 -0800

    update action

--- test_inputs/subdirectory/AppStringFeatureFlag.swift
+++ test_inputs/subdirectory/AppStringFeatureFlag.swift
@@ -9,7 +9,7 @@ enum AppStringFeatureFlag: String {
     var defaultValue: String {
         switch self {
         case .welcomeMessage: 
-            return "Welcome to the app 😂"
+            return "Welcome to the app ❤️"
         case .apiEndpoint:
             return "https://api.default.com"
         case .searchName:
